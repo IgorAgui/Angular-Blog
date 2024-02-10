aa76f767d784d04773c57be8fb5957354e7213bd
Igor <igoraguiarnft@gmail.com>
2024-02-10 04:13:10 -0300

    Version 0.1

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'NOVO HOMEM DE FERRO',
+    description: 'Marvel anuncia novo filme do homem de ferro, confira',
+    photoCover: 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/MSNBC/Components/Photo/_new/100506-ironman2-hmed.jpg',
+  },
+  {
+    id: '2',
+    title: 'Nova Série anunciada no DisneyPlus',
+    description: 'bla bla bla bla',
+    photoCover: 'https://f.i.uol.com.br/fotografia/2019/04/23/15560338625cbf314652022_1556033862_3x2_md.jpg',
+  },
+];
