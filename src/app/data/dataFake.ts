b1ebd80f26cf6f8aaaa72db3b15981a1b131ff39
Igor <igoraguiarnft@gmail.com>
2024-02-10 07:32:01 -0300

    att:description

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,74 @@
 export const dataFake = [
   {
     id: '1',
-    title: 'NOVO HOMEM DE FERRO',
-    description: 'Marvel anuncia novo filme do homem de ferro, confira',
-    photoCover: 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/MSNBC/Components/Photo/_new/100506-ironman2-hmed.jpg',
+    title: 'Angular New Version',
+    description: `Aprimoramentos de desempenho: O Angular 12 introduziu otimizações de compilação para reduzir o tamanho dos pacotes e melhorar os tempos de inicialização dos aplicativos. Isso incluiu melhorias na árvore de dependências e na compilação AOT (Ahead-of-Time).
+
+    Migração de TSLint para ESLint: O Angular 12 fez a transição do TSLint para o ESLint como a ferramenta padrão para linting de código. Isso proporciona um suporte mais amplo para regras de linting e integração mais suave com outras ferramentas de desenvolvimento.
+
+    Atualização da API para o HttpClient: A API do HttpClient no Angular foi atualizada para permitir a configuração mais fácil de interceptadores, simplificando assim a lógica de tratamento de solicitações HTTP em aplicativos.
+
+    Atualização do ngcc: O Angular Compatibility Compiler (ngcc) foi atualizado para melhorar a compatibilidade com bibliotecas e pacotes JavaScript de terceiros. Isso ajuda a garantir uma integração mais suave de bibliotecas externas com o Angular.
+
+    Novas opções de compilação para o Angular CLI: O Angular CLI (Command Line Interface) recebeu novas opções de compilação para permitir configurações mais personalizadas durante o processo de construção do aplicativo.
+
+    Melhorias na experiência do desenvolvedor: Foram feitas várias melhorias na experiência do desenvolvedor, incluindo atualizações nos recursos de autocompletar e sugestões do Angular Language Service, facilitando o desenvolvimento de aplicativos Angular.`,
+    photoCover:
+      'https://i.ytimg.com/vi/mVjYG9TSN88/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AH-CYAC0AWKAgwIABABGH8gEyhWMA8=&rs=AOn4CLAd8M7O1rK30HRH-9RXrVAjhjqCOA',
   },
   {
     id: '2',
-    title: 'Nova Série anunciada no DisneyPlus',
-    description: 'bla bla bla bla',
-    photoCover: 'https://f.i.uol.com.br/fotografia/2019/04/23/15560338625cbf314652022_1556033862_3x2_md.jpg',
+    title: 'Angular Version V11.0',
+    description: `O Angular 11 trouxe uma série de melhorias e novos recursos para facilitar o desenvolvimento de aplicativos web com Angular. Aqui está um resumo das principais atualizações:
+
+    Atualizações de desempenho: O Angular 11 introduziu várias melhorias de desempenho, incluindo otimizações de compilação e redução no tamanho dos pacotes gerados. Isso resultou em tempos de carregamento mais rápidos para os aplicativos Angular.
+
+    Webpack 5 suportado: O Angular 11 adicionou suporte para Webpack 5, o que trouxe melhorias significativas no empacotamento e no tempo de construção dos aplicativos, além de oferecer suporte para recursos mais recentes do JavaScript.
+
+    Atualizações de CLI: O Angular CLI (Command Line Interface) foi atualizado com novos recursos e melhorias, incluindo aprimoramentos na geração de código, na geração de bibliotecas e na criação de projetos.
+
+    Nova opção de migração automática: O Angular 11 introduziu uma nova opção de migração automática para ajudar os desenvolvedores a atualizarem seus aplicativos Angular para a versão mais recente com mais facilidade. Isso ajudou a simplificar o processo de atualização e reduzir o tempo necessário para migrar para uma nova versão.
+
+    Aprimoramentos na experiência do desenvolvedor: Foram feitas melhorias na experiência do desenvolvedor com aprimoramentos no Angular Language Service, fornecendo recursos de autocompletar e sugestões mais úteis durante o desenvolvimento.
+
+    Compatibilidade com TypeScript 4.0: O Angular 11 trouxe suporte para TypeScript 4.0, permitindo que os desenvolvedores aproveitassem os recursos mais recentes e melhorias de desempenho disponíveis nesta versão do TypeScript.`,
+    photoCover:
+      'https://arquivo.devmedia.com.br/noticias/documentacao/documentacao_angular-cli-como-rodar-testes-e-checar-o-codigo_38248.png',
+  },
+  {
+    id: '3',
+    title: 'Angular Version V10.0',
+    description: `O Angular 10 trouxe uma série de melhorias, correções de bugs e recursos novos para aprimorar a experiência de desenvolvimento e desempenho dos aplicativos Angular. Aqui está um resumo das principais atualizações:
+
+    Atualizações de dependências: O Angular 10 incluiu atualizações para várias dependências, incluindo TypeScript, RxJS e Node.js. Isso garantiu que os desenvolvedores pudessem aproveitar os recursos e melhorias mais recentes dessas bibliotecas e ferramentas.
+
+    Atualizações do Angular CLI: O Angular CLI (Command Line Interface) foi atualizado para oferecer suporte a novos recursos, como a capacidade de criar aplicativos com Schematics iniciais e aprimoramentos na geração de código.
+
+    Mudanças no Strict Mode: O Angular 10 introduziu mudanças no modo estrito (strict mode) por padrão, visando melhorar a segurança, manutenção e desempenho dos aplicativos Angular. Isso incluiu a ativação automática do strict mode durante a criação de novos aplicativos.
+
+    Aprimoramentos no Ivy: A nova versão continuou aprimorando o Angular Ivy, o novo mecanismo de renderização introduzido no Angular 9. Isso incluiu otimizações de desempenho, correções de bugs e suporte para novos recursos.
+
+    Atualizações de compilação: Foram feitas melhorias na compilação AOT (Ahead-of-Time) para reduzir o tempo de compilação e melhorar o desempenho dos aplicativos gerados.
+
+    Melhorias na acessibilidade: O Angular 10 incluiu melhorias na acessibilidade, garantindo que os aplicativos Angular possam ser facilmente acessados e utilizados por usuários com diferentes necessidades.`,
+    photoCover: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmR8DRTxxOATALcanOfYMltAGTZ0Jp1b91ww&usqp=CAU',
+  },
+  {
+    id: '4',
+    title: 'Angular Version V09.0',
+    description: `O Angular 9 trouxe várias melhorias significativas, correções de bugs e novos recursos para facilitar o desenvolvimento de aplicativos web com Angular. Aqui está um resumo das principais atualizações:
+
+    Introdução do Angular Ivy: A versão mais notável do Angular 9 foi a introdução do Angular Ivy como o novo mecanismo de renderização padrão. O Ivy trouxe melhorias significativas no desempenho, tamanho do código e capacidades de depuração em comparação com o mecanismo de renderização anterior.
+
+    Compatibilidade com TypeScript 3.6 e 3.7: O Angular 9 trouxe suporte para as versões mais recentes do TypeScript, permitindo que os desenvolvedores aproveitassem os recursos e melhorias disponíveis nessas versões.
+
+    Atualizações do Angular CLI: O Angular CLI (Command Line Interface) foi atualizado para fornecer suporte a novos recursos, como a capacidade de atualizar automaticamente a configuração do projeto para usar o Angular Ivy.
+
+    Aprimoramentos na produtividade do desenvolvedor: Foram feitas várias melhorias na produtividade do desenvolvedor, incluindo melhorias no Angular Language Service, que oferece recursos de autocompletar, sugestões e navegação de código no editor.
+
+    Remoção de APIs obsoletas: O Angular 9 removeu várias APIs obsoletas e não utilizadas, tornando o código mais limpo e facilitando a manutenção dos aplicativos.
+
+    Atualizações de dependências: O Angular 9 incluiu atualizações para várias dependências, como RxJS e Node.js, garantindo que os desenvolvedores possam aproveitar as versões mais recentes dessas bibliotecas.`,
+    photoCover: 'https://angular-academy.s3.amazonaws.com/thumbnails/advanced_angular-small-v3.png',
   },
 ];
